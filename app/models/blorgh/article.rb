02dcde8fb13deac4773263a595f6196f24d29c11
Ignacio Aliende García <ialiendeg@gmail.com>
2019-09-19 15:28:21 +0100

    Adds author class by parameter

--- app/models/blorgh/article.rb
+++ app/models/blorgh/article.rb
@@ -1,6 +1,6 @@
 module Blorgh
   class Article < ApplicationRecord
     has_many :comments
-    belongs_to :author, class_name: 'User'
+    belongs_to :author, class_name: Blorgh.author_class
   end
 end
